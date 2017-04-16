6f98a172a5ab4cb026ec096ce9741099ae795fdb
Alex <realtowz@yahoo.com>
2017-04-16 23:41:03 +0300

    remove comments

--- src/main/java/gr/charos/homeapp/contractions/App.java
+++ src/main/java/gr/charos/homeapp/contractions/App.java
@@ -7,10 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 
-/**
- * Hello world!
- *
- */
+
 @SpringBootApplication
 public class App implements CommandLineRunner {
 	
